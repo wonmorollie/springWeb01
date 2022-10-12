b3a36c807b2aa21b6274c3b7f0d761076b973779
Administrator <Administrator@304-17>
2022-10-12 17:49:17 +0900

    12345

---
+++ src/main/java/com/green/nowon/SpringWeb01MavenApplication.java
@@ -0,0 +1,13 @@
+package com.green.nowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb01MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeb01MavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/nowon/SpringWeb01MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.nowon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb01MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
